d9078149c2747b1d8008d66380c9d5de8ba9af9b
rntoscano <anny.toscano@hotmail.com.com>
2026-05-07 10:20:16 -0300

    Menu

---
+++ src/projeto_final_bloco_01/Menu.java
@@ -0,0 +1,84 @@
+package projeto_final_bloco_01;
+
+import java.util.Scanner;
+import projeto_final_bloco_01.util.Cores;
+
+
+public class Menu {
+	public static void main(String[] args) {
+
+		Scanner leia = new Scanner(System.in);
+
+		int opcao;
+
+		while (true) {
+			System.out.println(
+					Cores.BACKGROUND_YELLOW_PASTEL +
+					Cores.TEXT_BLACK_BOLD
+			);
+
+			System.out.println("*****************************************************");
+			System.out.println("                                                     ");
+			System.out.println("                 Padaria Doce Sonho                  ");
+			System.out.println("                                                     ");
+			System.out.println("*****************************************************");
+			System.out.println("                                                     ");
+			System.out.println("            1 - Adicionar Produto                    ");
+			System.out.println("            2 - Atualizar Produto                    ");
+			System.out.println("            3 - Listar Produtos                      ");
+			System.out.println("            4 - Excluir Produto                      ");
+			System.out.println("            0 - Sair                                 ");
+			System.out.println("                                                     ");
+			System.out.println("*****************************************************");
+			System.out.println("Entre com a opção desejada:                          ");
+			System.out.println("                                                     " + Cores.TEXT_RESET);
+			
+		
+
+			opcao = leia.nextInt();
+
+			if (opcao == 0) {
+
+				System.out.println(
+					Cores.BACKGROUND_YELLOW_PASTEL +
+					Cores.TEXT_BLACK_BOLD
+			);
+				System.out.println("\n    Padaria Doce Sonho - Produzindo Bons Sonhos!     ");
+				sobre();
+                 leia.close();
+				System.exit(0);
+			}
+
+			switch (opcao) {
+				case 1:
+					System.out.println("Adicionar Produto\n\n");
+
+					break;
+				case 2:
+					System.out.println("Atualizar Produto\n\n");
+
+					break;
+				case 3:
+					System.out.println("Listar Produtos\n\n");
+
+					break;
+				case 4:
+					System.out.println("Excluir Produto\n\n");
+
+
+					break;
+				default:
+					System.out.println("\nOpção Inválida!\n");
+					break;
+			}
+		}
+	}
+    
+	public static void sobre() {
+		System.out.println("\n*****************************************************");
+		System.out.println("Projeto Desenvolvido por: Riane Toscano              ");
+		System.out.println("anny.toscano@hotmail.com                             ");
+		System.out.println("https://github.com/rntoscano                         ");
+		System.out.println("*****************************************************");
+	}
+}
